57727ba9dba1df4c5ae1cad485b12f0da9fa5515
Swam244 <123237756+Swam244@users.noreply.github.com>
2023-05-31 08:14:33 +0530

    Update qRCode.py

--- qRCode.py
+++ qRCode.py
@@ -1,14 +1,5 @@
 from pyqrcode import create
 from sys import argv
-query = "Nirav"
+query = ""
 qr = create('{}'.format(query))
 qr.png('qrcode_1{}.png'.format(query).replace('www.',''), scale=5)
-
-
-
-# from weather import Weather, Unit
-# key = "xF3ItVGYToPBaEn674GSGEPJ824CLAdgJ4mhthX5"
-# weather = Weather(unit=Unit.CELSIUS)
-# lookup = weather.lookup_by_latlng(lat=20.904221,lng=74.774895)
-# condition = lookup.condition
-# print(condition.text)
